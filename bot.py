6f05f301fdc37eae49cfbb3f15f2db9839564634
umbreon70000 <umbreon75000@gmail.com>
2019-06-20 18:46:36 +0100

    Changing event time

--- bot.py
+++ bot.py
@@ -297,7 +297,7 @@ async def on_message(message):
         s = utcTime.strftime('%Y-%m-%d %H:%M:%S.%f')
         newUtc = s[:-7] #Removing the milliseconds
         strUtc = str(newUtc)
-        eventTime = '2019-06-26 07:59:00' #This is the time when the event ends, the one I change
+        eventTime = '2019-06-24 07:59:00' #This is the time when the event ends, the one I change
         FMT = '%Y-%m-%d %H:%M:%S'
         if eventTime > strUtc:
             tdelta = datetime.strptime(eventTime, FMT) - datetime.strptime(strUtc, FMT)
